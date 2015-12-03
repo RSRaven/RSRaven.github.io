16e737194ea3228934a012f7461f44ca44c17107
Nikita Lovygin <rsraven@yandex.ru>
2015-12-03 09:33:49 +0200

    ДЗ10-11. Допилил

--- GoIT/markup/lesson10-11/Logon/js/script.js
+++ GoIT/markup/lesson10-11/Logon/js/script.js
@@ -1,24 +1,45 @@
-var names = [];
-var currentName;
+var users = [];
+var user;
 
 for (var i = 0; i < 5; i++) {
-	names[i] = 	prompt('Введите ' + (i + 1) + ' имя:');
+
+	while (true) {
+
+		user = prompt('Введите ' + (i + 1) + ' имя:');
+
+		if ((user != 'null') && (user != '')) {
+
+			break;
+
+		}
+
+		alert('Введите имя!');
+
+	}
+
+	users[i] = user;
+
 }
 
-currentName = prompt('Введите имя пользователя:');
+var currentUser = prompt('Введите имя пользователя:');
+var flag = false;
 
-for (var i = 0; i < names.length; i++) {
+for (var i = 0; i < users.length; i++) {
 
-	if (currentName == names[i]){
+	if (currentUser == users[i]){
 
-		alert(currentName + ', вы успешно вошли!');
-		break;
-
-	} else {
-
-		alert('Пользователь не зарегистрирован');
+		flag = true;
 		break;
 
 	}
-	 
+}
+
+if (flag) {
+
+	alert(currentUser + ', вы успешно вошли!');
+
+} else {
+
+	alert('Пользователь не зарегистрирован');
+	
 }
