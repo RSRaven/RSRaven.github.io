40ecb291aead4d40c9a0f36f9969b7e5e8ef4963
RSRaven <rsraven@yandex.ru>
2016-01-31 19:28:22 +0200

    Finished JS #15-16.

---
+++ GoIT/markup/js_15-16/js/script.js
@@ -0,0 +1,118 @@
+$(function() {
+
+var $searcher = $('.searcher');
+
+var $ajaxQuery = function(i){
+
+    var $text = $('#query-search');
+    var url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=8&start=' + i*8 + '&q='+ encodeURIComponent($text.val()) + '&callback=GoogleCallback&context=?';
+    var $wrapper = $('.wrapper');
+    $('.results').remove();
+
+    $.ajax({
+        url: url,
+        dataType : "jsonp",
+        success: function(data) {
+
+            var results = document.createElement('div');
+            results.classList.add('results');
+            var ul = document.createElement('ul');
+
+            var moreResults = document.createElement('div');
+            moreResults.classList.add('more-results');
+            var h3 = document.createElement('h3');
+            h3.innerHTML = ('More results...');
+            var p = document.createElement('p');
+            p.classList.add('more-results-list');
+
+            for (i = 0; i < 8; i++) {
+                p.innerHTML += '<a href="http://more... :)" class="search-more">' + (i+1) + '</a>';
+            }
+
+            $.each(data.results, function(i, val) {
+
+                var li = document.createElement('li');
+                li.innerHTML = ('<h3><a href="' + val.url + '">' + val.title + '</a></h3><p>' + val.content + '</p>');
+                ul.appendChild(li);
+            });
+
+            results.appendChild(ul);
+            $wrapper.append(results);
+            moreResults.appendChild(h3);
+            moreResults.appendChild(p);
+            results.appendChild(moreResults);
+
+            var setAnchors = function() {
+                $anchors = $('.search-more');
+                $.each($anchors, function(i) {
+                    $anchors[i].addEventListener('click', function(e) {
+                        e.preventDefault();
+                        $ajaxQuery(i);
+                    });
+                });
+            };
+
+            setAnchors();
+        }
+    });
+
+};
+
+$searcher.submit(function(e) {
+
+    e.preventDefault();
+
+    $ajaxQuery(0);
+});
+
+// second part
+
+var human = {
+    name: 'Humanoid',
+    age: 18,
+    sex: 'male',
+    height: '180',
+    weight: 70
+};
+
+var worker = {
+    workPlace: 'Luxoft',
+    salary: 5000,
+    work: function() {
+        alert('Скоро пятница!');
+    }
+};
+
+var student = {
+    studyPlace: 'GoIT',
+    grants: 1000,
+    watchTV: function() {
+        alert('Давай ещё серию, забей на пары!');
+    }
+};
+
+Object.setPrototypeOf(worker, human);
+Object.setPrototypeOf(student, human);
+
+var Vasiliy = Object.create(student);
+Vasiliy.name = 'Vasiliy';
+Vasiliy.age = 20;
+Vasiliy.weight = 100;
+
+var Pyotr = Object.create(worker, {
+    name: {value: 'Pyotr'},
+    salary: {value: 10000},
+    age: {value: 45},
+});
+
+console.log('human', human);
+console.log('student ', student);
+console.log('worker ', worker);
+console.log('Student ', Vasiliy.name + ' studying in ' + Vasiliy.studyPlace + ' and recieving ' + Vasiliy.grants + ' grants, his age ' + Vasiliy.age + ' and height is ' + Vasiliy.height);
+console.log('Object Name = ', Pyotr.name + '  Age = ' + Pyotr.age + '  Working at = ' + Pyotr.workPlace + '  Salary = ' + Pyotr.salary + '  Height = ' + Pyotr.height);
+
+});
+
+function GoogleCallback (func, data) {
+    window[func](data);
+}
