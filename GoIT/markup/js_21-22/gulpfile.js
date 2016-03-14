dd3fc1deeb8bcd9cafbe5c7dce02aac4325c2474
Nikita Lovygin <rsraven@yandex.ru>
2016-03-14 10:47:06 +0200

    JS# 21-22. Implemented ES6 to script from JS# 13-14

--- GoIT/markup/js_21-22/gulpfile.js
+++ GoIT/markup/js_21-22/gulpfile.js
@@ -2,6 +2,12 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const mocha = require('gulp-mocha');
 const watch = require('gulp-watch');
+const rimraf = require('rimraf');
+
+gulp.task('build:static', () =>
+    gulp.src('src/**/*.{html,css}')
+        .pipe(gulp.dest('build'))
+);
 
 gulp.task('babel', () =>
     gulp.src('src/js/**/*.js')
@@ -11,15 +17,24 @@ gulp.task('babel', () =>
         .pipe(gulp.dest('build/js'))
 );
 
+gulp.task('build', ['build:static', 'babel']);
+
 gulp.task('mocha', () => {
     return gulp.src('test/**/*.js', {read: false})
         .pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('watch', () => {
+    watch(['src/**/*.{html,css}'], function(event, cb) {
+        gulp.start('build:static');
+    });
     watch(['src/js/**/*.js', 'test/**/*.js'], function(event, cb) {
         gulp.start('babel', 'mocha');
     });
 });
 
-gulp.task('default', ['babel', 'mocha', 'watch']);
+gulp.task('clean', function (cb) {
+    rimraf('build', cb);
+});
+
+gulp.task('default', ['build', 'mocha', 'watch']);
